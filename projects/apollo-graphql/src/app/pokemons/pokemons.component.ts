401788add37529affcb0ed995fac9e718522c6a4
Richard Lindner <richard.lnnr@gmail.com>
2021-07-13 15:06:18 -0300

    feat: add pokemons route
    Add pokemons route to handle all the fetching options of the api

---
+++ projects/apollo-graphql/src/app/pokemons/pokemons.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemons',
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.scss']
+})
+export class PokemonsComponent {
+
+}
